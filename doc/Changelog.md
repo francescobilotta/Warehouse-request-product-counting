170b02f1e0502ddffd467e5aaade00c6d61a6b8d
I.M. Beniamin <darkengineer@protonmail.com>
2021-07-21 15:21:22 +0200

    [Project setup]

---
+++ doc/Changelog.md
@@ -0,0 +1,10 @@
+## 1. [Project setup]
+
+####- Created database connection
+####- Applied bootstrap template
+####- Created the basic structure of the table
+####- Added jQuery bindings
+![Project setup](changelog_images/1_1.png)<br/>
+_image of project setup_
+![Structure](changelog_images/1_2.png)<br/>
+_structured file_
--- src/pages/office/Homepage.php
+++ src/pages/office/Homepage.php
@@ -1,47 +1,151 @@
 
 <!DOCTYPE html>
 <html lang="it">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="../../assets/img/favicon.png">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="../../assets/img/favicon.png">
 
-    <title>Warehouse counter</title>
-    <!-- Bootstrap -->
-      <!-- Latest compiled and minified CSS -->
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-      <!-- jQuery library -->
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-      <!-- Popper JS -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-      <!-- Latest compiled JavaScript -->
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <title>Warehouse counter</title>
+        <!-- Bootstrap -->
 
-    <!-- jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
-    <!-- Common styles -->
-      <link href="../../assets/common.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="./res/css/Homepage.css" rel="stylesheet">
-  </head>
-  <body>
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+
+
+        <!-- Common styles -->
+        <link href="../../assets/common.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="./res/css/Homepage.css" rel="stylesheet">
+    </head>
+    <body>
 
     <main role="main" class="container">
 
-    <?php
-        $host="localhost";
-        $username="root";
-        $password="basilicagoiano";
-        $database="processi";
-        $mysqli = new mysqli($host, $username, $password, $database, 3306) or die("Errore nella connessione MySQL");
-      echo $mysqli->host_info . "\n";
-    ?>
+        <?php
+            $host="localhost";
+            $username="root";
+            $password="basilicagoiano";
+            $database="processi";
+            $mysqli = new mysqli($host, $username, $password, $database, 3306) or die("Errore nella connessione MySQL");
+          echo $mysqli->host_info . "\n";
+        ?>
 
-      <div class="starter-template">
-        <h1>Office</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
+        <div class="header">
+            <h1>Office Section</h1>
+            <p class="lead">Section Title</p>
+        </div>
+
+        <div class="sub-header">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="filters" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Filters
+            </button>
+            <div class="dropdown-menu" aria-labelledby="filters">
+                <a class="dropdown-item" href="#">Most Recent</a>
+                <a class="dropdown-item" href="#">Oldest</a>
+                <a class="dropdown-item" href="#">Upcoming due date</a>
+
+                <a class="dropdown-item" href="#">
+                    <label class="form-check-label" for="flexCheckChecked">
+                        <input class="" type="checkbox" value="showClosed" id="flexCheckChecked">
+                        Show closed
+                    </label>
+                </a>
+            </div>
+            <button type="button" class="btn btn-primary">Add Request</button>
+        </div>
+        <div class="request-table">
+            <table class="table">
+                <thead class="table-head">
+                <tr class="header-row">
+                    <td class="table-header">Modify</td>
+                    <td class="table-header">Status</td>
+                    <td class="table-header">Product</td>
+                    <td class="table-header">Flavour</td>
+                    <td class="table-header">Notes</td>
+                    <td class="table-header">Request Date</td>
+                    <td class="table-header">Due Date</td>
+                    <td class="table-header">Closed Date</td>
+                    <td class="table-header">Last Count</td>
+                </tr>
+                </thead>
+
+                <tbody class="table-body">
+                    <tr class="table-row">
+                        <td class="table-data">
+                            <button type="button" class="btn btn-default" aria-label="Left Align">
+                                <span class="glyphicon glyphicon-pencil"></span>
+                            </button>
+                        </td>
+                        <td class="table-data">
+                            <button type="button" class="btn btn-default" aria-label="Left Align">
+                                <span class="glyphicon glyphicon-remove"></span>
+                            </button>
+                        </td>
+                        <td class="table-data"><span>Product Name</span></td>
+                        <td class="table-data"><span>Red</span></td>
+                        <td class="table-data"></td>
+                        <td class="table-data"><span>Some </span></td>
+                        <td class="table-data"><span>Some Date</span></td>
+                        <td class="table-data"><span>Some Date</span></td>
+                        <td class="table-data"><span>Some Date</span></td>
+                        <td class="table-data"></td>
+                    </tr>
+                    <tr class="table-row">
+                        <td class="table-data">
+                            <button type="button" class="btn btn-default" aria-label="Left Align">
+                                <span class="glyphicon glyphicon-pencil"></span>
+                            </button>
+                        </td>
+                        <td class="table-data">
+                            <button type="button" class="btn btn-default" aria-label="Left Align">
+                                <span class="glyphicon glyphicon-remove"></span>
+                            </button>
+                        </td>
+                        <td class="table-data"><span>Product Name</span></td>
+                        <td class="table-data"><span>Blue</span></td>
+                        <td class="table-data"></td>
+                        <td class="table-data"><span>Some </span></td>
+                        <td class="table-data"><span>Some Date</span></td>
+                        <td class="table-data"><span>Some Date</span></td>
+                        <td class="table-data"><span>Some Date</span></td>
+                        <td class="table-data"><span>28</span></td>
+                    </tr>
+                    <tr class="table-row">
+                        <td class="table-data">
+                            <button type="button" class="btn btn-default" aria-label="Left Align">
+                                <span class="glyphicon glyphicon-pencil"></span>
+                            </button>
+                        </td>
+                        <td class="table-data">
+                            <button type="button" class="btn btn-default" aria-label="Left Align">
+                                <span class="glyphicon glyphicon-ok"></span>
+                            </button>
+                        </td>
+                        <td class="table-data"><span>Product Name</span></td>
+                        <td class="table-data"></td>
+                        <td class="table-data"><span>Some notes</span></td>
+                        <td class="table-data"><span>Some </span></td>
+                        <td class="table-data"><span>Some Date</span></td>
+                        <td class="table-data"><span>Some Date</span></td>
+                        <td class="table-data"><span>Some Date</span></td>
+                        <td class="table-data"><span>32</span></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
     </main>
 
