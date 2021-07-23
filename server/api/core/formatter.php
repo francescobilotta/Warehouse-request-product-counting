bf72832b93b2f5eb4d5453b027b3b88e66d58ff7
I.M. Beniamin <darkengineer@protonmail.com>
2021-07-23 09:40:38 +0200

    refactored the handling of the query results in the api

--- server/api/core/formatter.php
+++ server/api/core/formatter.php
@@ -1,7 +1,7 @@
 <?php
-$data = $_GET['q_result'];
+$data = array("results"=>$_GET['q_result']);
 
-if ($data) { $data['status'] = 'ok'; }
-else { $data = array("status"=>"bad_result"); }
+if ($data['results']) { $data['status'] = 'ok'; }
+else { $data["status"] = "bad_result"; }
 
 echo json_encode($data);
