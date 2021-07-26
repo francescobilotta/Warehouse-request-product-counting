3f124bd7428910395384ca947a0dd5b5d354325d
I.M. Beniamin <darkengineer@protonmail.com>
2021-07-26 14:15:01 +0200

    abstracted ajax calls into a library and fixed office_homepage.html accordingly.

---
+++ client/src/assets/js/request.js
@@ -0,0 +1,35 @@
+
+class Requester {
+    constructor(query_data, results_callback, req_type="GET", req_url="../../../../server/api/request_receiver.php", req_ret_type="json") {
+        this.query_data = query_data;
+        this.results_callback = results_callback;
+        this.req_type = req_type;
+        this.req_url = req_url;
+        this.req_ret_type = req_ret_type;
+    }
+
+    query() {
+        let request = $.ajax(
+            {
+                type: this.req_type,
+                url: this.req_url,
+                dataType: this.req_ret_type,
+                data: this.query_data,
+                success: (data) => {
+                    if (data.status === "ok") {
+                        this.result = data;
+                        this.results_callback(this.result)
+                    }
+                    else {
+                        console.log("Error, the api has encountered a problem. Status: " + data.status);
+                        this.result = undefined;
+                    }
+                },
+                error: (error) => {
+                    console.log("Error, the ajax request could not be completed. Message: " + error);
+                    this.result = undefined;
+                }
+            }
+        );
+    }
+}
