415c38b749a52ef564e4cfd3d336d992422624e0
I.M. Ben <darkengineer@protonmail.com>
2021-07-28 15:34:49 +0200

    finished refactoring the ajax api

--- client/src/assets/js/request.js
+++ client/src/assets/js/request.js
@@ -1,25 +1,27 @@
 class Requester {
   constructor(
-    query_data,
     results_callback,
     req_type = "GET",
     req_url = "../../../../server/api/request_receiver.php",
     req_ret_type = "json"
   ) {
-    this.query_data = query_data;
+    this.query_data = undefined;
     this.results_callback = results_callback;
     this.req_type = req_type;
     this.req_url = req_url;
     this.req_ret_type = req_ret_type;
   }
 
-  query() {
+  query(ajax_data=this.query_data) {
+    if (!ajax_data) {
+      alert("The ajax request has no data");
+    }
     let request = $.ajax({
       async: true,
       type: this.req_type,
       url: this.req_url,
       dataType: this.req_ret_type,
-      data: this.query_data,
+      data: ajax_data,
       success: (data) => {
         if (data.status === "ok") {
           this.result = data;
@@ -40,5 +42,12 @@ class Requester {
 
 class q_data {
   constructor() {
+  }
+}
+
+class ajax_data {
+  constructor(q_name, q_data) {
+    this.q_name = q_name;
+    this.q_data = q_data;
   }
 }
