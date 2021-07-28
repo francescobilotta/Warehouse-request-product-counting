f8c33f37b98377afc1a8ec118c27efe4e0e8d392
Francesco Bilotta <bilottafrancesco.business@gmail.com>
2021-07-28 11:31:27 +0200

    Working

---
+++ client/src/assets/js/append_img_request_state.js
@@ -0,0 +1,13 @@
+function append_img_request_state(divToAppend, url, imgSize, title) {
+  var divToAppend = divToAppend;
+  var url = url;
+  var imgSize = imgSize;
+  var title = title;
+  alert("#");
+  $("#img_request_state").remove();
+  $("#" + divToAppend + "").append(
+    $("<div id='img_request_state'>").append(
+      $("<img src='" + url + "' height=' " + imgSize + " ' width=' " + imgSize + " ' title='" + title + "'>")
+    )
+  );
+}
