a33606727031c66526202b248db33ad202426bad
I.M. Ben <darkengineer@protonmail.com>
2021-07-28 12:37:48 +0200

    debugging

--- server/api/core/formatter.php
+++ server/api/core/formatter.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-echo $_SESSION['q_result'];
+echo implode("---",$_SESSION['q_result']);
 $data = array("results"=>json_encode($_SESSION['q_result']));
 
 $data['status'] = 'ok';
