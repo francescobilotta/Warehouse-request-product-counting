3ea858e0f681373ffd12858e2f2d761993523f33
Francesco Bilotta <bilottafrancesco.business@gmail.com>
2021-07-28 16:43:15 +0200

    Almost finished main UI Fixed query wih unneccessary datas Update query doesn't work Going to work on warehouse

--- client/src/assets/js/table_generator.js
+++ client/src/assets/js/table_generator.js
@@ -13,31 +13,69 @@ function table_maker(table_data, destination_id, headers) {
   for (const row of table_data) {
     let current_row = $("<tr>").addClass("table-row").attr("id", row["requestId"]);
 
+    var image_size = "50px";
+
     Object.keys(headers).forEach(function (key) {
-      if (headers[key]) {
-        switch (row[headers[key]]) {
-          case "countRequest":
-            current_row.append(
-              $("<td>").append($("<img src='../../assets/img/countRequest.png' height='80px' width='80px' title='Asked for count'>"))
-            );
-            break;
-          case "countDone":
-            current_row.append(
-              $("<td>").append($("<img src='../../assets/img/countDone.png' height='80px' width='80px' title='Count done'>"))
-            );
-            break;
-          case "recountRequest":
-            current_row.append(
-              $("<td>").append($("<img src='../../assets/img/recountRequest.png' height='80px' width='80px' title='Asked for recount'>"))
-            );
-            break;
-          case "recountDone":
-            current_row.append(
-              $("<td>").append($("<img src='../../assets/img/recountDone.png' height='80px' width='80px' title='Recount done'>"))
-            );
-            break;
-          default:
-            current_row.append($("<td>").text(row[headers[key]]));
+      //CHECK isClosed
+      if (headers[key] == "isClosed" && row[headers[key]] == 1) {
+        current_row.append(
+          $("<td>").append(
+            $("<img src='../../assets/img/closed_icon.png' height='" + image_size + "' width='" + image_size + "' title='Count done'>")
+          )
+        );
+      } else {
+        //CHECK requestState
+        if (headers[key]) {
+          switch (row[headers[key]]) {
+            case "countRequest":
+              current_row.append(
+                $("<td>").append(
+                  $(
+                    "<img src='../../assets/img/countRequest.png' height='" +
+                      image_size +
+                      "' width='" +
+                      image_size +
+                      "' title='Asked for count'>"
+                  )
+                )
+              );
+              break;
+            case "countDone":
+              current_row.append(
+                $("<td>").append(
+                  $("<img src='../../assets/img/countDone.png' height='" + image_size + "' width='" + image_size + "' title='Count done'>")
+                )
+              );
+              break;
+            case "recountRequest":
+              current_row.append(
+                $("<td>").append(
+                  $(
+                    "<img src='../../assets/img/recountRequest.png' height='" +
+                      image_size +
+                      "' width='" +
+                      image_size +
+                      "' title='Asked for recount'>"
+                  )
+                )
+              );
+              break;
+            case "recountDone":
+              current_row.append(
+                $("<td>").append(
+                  $(
+                    "<img src='../../assets/img/recountDone.png' height='" +
+                      image_size +
+                      "' width='" +
+                      image_size +
+                      "' title='Recount done'>"
+                  )
+                )
+              );
+              break;
+            default:
+              current_row.append($("<td>").text(row[headers[key]]));
+          }
         }
       }
     });
