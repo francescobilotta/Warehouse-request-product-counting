76e33c0fd39d7ebd617c2d19f04a01afc28016b1
I.M. Ben <darkengineer@protonmail.com>
2021-07-28 12:09:49 +0200

    Debugging API errors

--- server/api/core/db_connectors/oracle.php
+++ server/api/core/db_connectors/oracle.php
@@ -1,7 +1,11 @@
 <?php
 namespace oracleDB;
-use Exception;
 session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+use Exception;
+
 class OracleDB {
     static function init()
     {
