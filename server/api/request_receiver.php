a5d4dcd01e1a9e6a8c730665ff2c93786f1de6e8
I.M. Beniamin <darkengineer@protonmail.com>
2021-07-29 10:50:32 +0200

    testing

--- server/api/request_receiver.php
+++ server/api/request_receiver.php
@@ -8,9 +8,9 @@ use mysql\MYSQL;
 use oracleDB\OracleDB;
 
 if ($_GET['debug']) {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+//    ini_set('display_errors', 1);
+//    ini_set('display_startup_errors', 1);
+//    error_reporting(E_ALL);
 }
 
 /**
