1e5535b462db73163799e0bfc122b7d193c39809
Francesco Bilotta <bilottafrancesco.business@gmail.com>
2021-07-29 15:53:53 +0200

    Updated queries Working API perfectly Warehouse completed

--- client/src/assets/js/queries.js
+++ client/src/assets/js/queries.js
@@ -19,3 +19,68 @@ function closeRequest() {
 function askRecount() {
   //TODO
 }
+
+function lastToPrevious(requestId) {
+  const table_data_requester = new Requester(function (result) {
+    console.log(`Moved lastCount of request ${requestId} from lastCount to previousCount`);
+  });
+  table_data_requester.query({
+    q_name: "last_to_previous",
+    q_data: {
+      "f.requestId": requestId,
+    },
+  });
+  return true;
+}
+function warehouseStateToCountDone(requestId) {
+  const table_data_requester = new Requester(function (result) {
+    console.log(`Request ${requestId} state changed to countDone`);
+  });
+  table_data_requester.query({
+    q_name: "warehouse_state_to_countDone",
+    q_data: {
+      "f.requestId": requestId,
+    },
+  });
+  return true;
+}
+function warehouseStateToRecountDone(requestId) {
+  const table_data_requester = new Requester(function (result) {
+    console.log(`Request ${requestId} state changed to recountDone`);
+  });
+  table_data_requester.query({
+    q_name: "warehouse_state_to_recountDone",
+    q_data: {
+      "f.requestId": requestId,
+    },
+  });
+  return true;
+}
+
+function warehouseUpdateRequests(requestId, lastCount) {
+  const table_data_requester = new Requester(function (result) {
+    console.log(`Request ${requestId} updated`);
+  });
+  table_data_requester.query({
+    q_name: "warehouse_update_count",
+    q_data: {
+      "d.lastCount": lastCount,
+      "f.requestId": requestId,
+    },
+  });
+}
+
+function warehouseCount(requestId, lastCount, requestState) {
+  $("#edit-popup").hide();
+  if (lastToPrevious(requestId)) {
+    if (requestState == "countRequest") {
+      if (warehouseStateToCountDone(requestId)) {
+        warehouseUpdateRequests(requestId, lastCount);
+      }
+    } else if (requestState == "recountRequest") {
+      if (warehouseStateToRecountDone(requestId)) {
+        warehouseUpdateRequests(requestId, lastCount);
+      }
+    }
+  }
+}
